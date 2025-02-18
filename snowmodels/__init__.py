d7646264ac6b35a53de0f1324e4ad5fcd0e1fd22
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-02-18 14:46:13 -0700

    updated init

--- snowmodels/__init__.py
+++ snowmodels/__init__.py
@@ -1,5 +1,18 @@
-"""Top-level package for snowmodels."""
+"""
+Top-level package for snowmodels.
+"""
 
 __author__ = """Ibrahim Alabi"""
 __email__ = "ibrahimolalekana@u.boisestate.edu"
 __version__ = "0.0.1"
+
+
+_submodules = [
+    'swe',
+    'density'
+]
+
+__all__ = _submodules
+
+def __dir__():
+    return __all__

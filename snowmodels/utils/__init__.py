8432eaa4ec04eed80bc7316ea363dd52400684c2
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-02-21 02:51:17 -0700

    added ComprehensiveOptimizer to init

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -3,8 +3,8 @@ The :mod:`snowmodels.utils` module includes various utilities.
 """
 
 from ._conversions import ConvertData
-from ._ml_model_tuner import DefaultTuner
 from ._snotel_data_download import SnotelData
+from ._ml_model_tuner import DefaultTuner, ComprehensiveOptimizer
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, split_data, compare_multiple_models
 
@@ -18,6 +18,7 @@ __all__ = [
     'validate_DOY',
     'evaluate_model',
     'compare_multiple_models',
+    'ComprehensiveOptimizer',
     'preprocess_set_to_nan',
     'calculate_lagged_vars',
     'calculate_pptwt',

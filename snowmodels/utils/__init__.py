64d710b8aa101522be92d1b1ccf87c0e26289b9f
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-02-21 00:33:30 -0700

    added DefaultTuner to init

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -3,6 +3,7 @@ The :mod:`snowmodels.utils` module includes various utilities.
 """
 
 from ._conversions import ConvertData
+from ._ml_model_tuner import DefaultTuner
 from ._snotel_data_download import SnotelData
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, split_data, compare_multiple_models
@@ -13,6 +14,7 @@ __all__ = [
     'SnotelData',
     'split_data',
     'ConvertData',
+    'DefaultTuner',
     'validate_DOY',
     'evaluate_model',
     'compare_multiple_models',

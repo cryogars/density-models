3a9666086231f520ee3d92983d493e3e728e8ae9
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-02-23 11:16:51 -0700

    updated init with ClimateOptimizer

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -4,7 +4,7 @@ The :mod:`snowmodels.utils` module includes various utilities.
 
 from ._conversions import ConvertData
 from ._snotel_data_download import SnotelData
-from ._ml_model_tuner import DefaultTuner, ComprehensiveOptimizer
+from ._ml_model_tuner import DefaultTuner, ComprehensiveOptimizer, ClimateOptimizer
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, split_data, compare_multiple_models
 
@@ -17,6 +17,7 @@ __all__ = [
     'DefaultTuner',
     'validate_DOY',
     'evaluate_model',
+    'ClimateOptimizer',
     'compare_multiple_models',
     'ComprehensiveOptimizer',
     'preprocess_set_to_nan',

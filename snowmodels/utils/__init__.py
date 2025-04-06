f0134f6bfbcfce70160de40fd30a8bd9742c3aca
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-04-05 20:59:38 -0600

    updated init

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -4,6 +4,7 @@ The :mod:`snowmodels.utils` module includes various utilities.
 
 from ._conversions import ConvertData
 from ._snotel_data_download import SnotelData
+from ._ml_model_transferability import plot_learning_curve
 from ._ml_model_tuner import DefaultTuner, ComprehensiveOptimizer, ClimateOptimizer
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, split_data, compare_multiple_models
@@ -18,6 +19,7 @@ __all__ = [
     'validate_DOY',
     'evaluate_model',
     'ClimateOptimizer',
+    'plot_learning_curve',
     'compare_multiple_models',
     'ComprehensiveOptimizer',
     'preprocess_set_to_nan',

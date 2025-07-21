ff7aec0de40c32eb4dd42ce29ca94fe7a001e2b3
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-07-21 16:43:15 -0600

    removed split data because refactored

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -7,13 +7,12 @@ from ._snotel_data_download import SnotelData
 from ._ml_model_transferability import plot_learning_curve
 from ._ml_model_tuner import DefaultTuner, ComprehensiveOptimizer, ClimateOptimizer
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
-from ._model_utils import validate_DOY, evaluate_model, split_data, compare_multiple_models
+from ._model_utils import validate_DOY, evaluate_model, compare_multiple_models
 
 
 
 __all__ = [
     'SnotelData',
-    'split_data',
     'ConvertData',
     'DefaultTuner',
     'validate_DOY',

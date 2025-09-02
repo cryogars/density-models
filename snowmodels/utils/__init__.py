aef43bf909c9327c2aedef006cb97302cda89199
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-09-02 09:34:51 -0600

    updated __all__

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -5,15 +5,16 @@ The :mod:`snowmodels.utils` module includes various utilities.
 from ._conversions import ConvertData
 from ._snotel_data_download import SnotelData
 from ._ml_model_transferability import plot_learning_curve
-from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, compare_multiple_models, SplitterFactory
+from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector, GlobalConfig
 
 
 
 __all__ = [
     'load_data',
     'SnotelData',
+    'GlobalConfig',
     'ConvertData',
     'validate_DOY',
     'evaluate_model',

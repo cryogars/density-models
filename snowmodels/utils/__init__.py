310e0019f55a3056d0365ce52af44bd1d6511483
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-09-02 09:38:32 -0600

    updated __all__

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -7,14 +7,14 @@ from ._snotel_data_download import SnotelData
 from ._ml_model_transferability import plot_learning_curve
 from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
 from ._model_utils import validate_DOY, evaluate_model, compare_multiple_models, SplitterFactory
-from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector, GlobalConfig
+from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector, load_config
 
 
 
 __all__ = [
     'load_data',
     'SnotelData',
-    'GlobalConfig',
+    'load_config',
     'ConvertData',
     'validate_DOY',
     'evaluate_model',

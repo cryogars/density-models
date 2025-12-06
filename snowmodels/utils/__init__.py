50be00aac22bb2ef9b7d8aacf85d6ee7a5c88a66
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2025-12-06 07:50:16 -0700

    updated init for multiple lags

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -5,7 +5,7 @@ The :mod:`snowmodels.utils` module includes various utilities.
 from ._conversions import ConvertData
 from ._snotel_data_download import SnotelData
 from ._ml_model_transferability import plot_learning_curve
-from ._other_utils import preprocess_set_to_nan, calculate_lagged_vars, calculate_pptwt
+from ._other_utils import preprocess_set_to_nan, add_multiple_lags, calculate_pptwt
 from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector, load_config
 from ._model_utils import validate_DOY, evaluate_model, compare_multiple_models, SplitterFactory, SplitResult
 
@@ -21,9 +21,9 @@ __all__ = [
     'evaluate_model',
     'calculate_pptwt',
     'SplitterFactory',
+    'add_multiple_lags',
     'plot_learning_curve',
     'preprocess_set_to_nan',
-    'calculate_lagged_vars',
     'ecnoder_preprocessor',
     'compare_multiple_models', 
     'model_variant_selector'

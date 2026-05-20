529962f88d64a56bb345a6074207454bb65023c9
Ibrahim Alabi <ibrahimolalekana@u.boisestate.edu>
2026-05-19 19:10:13 -0600

    fixed Libcomp

--- snowmodels/utils/__init__.py
+++ snowmodels/utils/__init__.py
@@ -4,7 +4,6 @@ The :mod:`snowmodels.utils` module includes various utilities.
 
 from ._conversions import ConvertData
 from ._snotel_data_download import SnotelData
-from ._ml_model_transferability import plot_learning_curve
 from ._other_utils import preprocess_set_to_nan, add_multiple_lags, calculate_pptwt
 from ._hyperopt_utils import ecnoder_preprocessor, load_data, model_variant_selector, load_config
 from ._model_utils import validate_DOY, evaluate_model, compare_multiple_models, SplitterFactory, SplitResult
